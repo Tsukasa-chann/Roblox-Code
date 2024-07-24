570038a83833e3f659293e97ca1c06d981f84632
Tsukasa-chann <85534233+Tsukasa-chann@users.noreply.github.com>
2024-07-24 17:59:24 +0800

    source.lua

--- source.lua
+++ source.lua
@@ -9,6 +9,8 @@ if game.PlaceId == 18153052081 then
     _G.autoBoss1 = true
     _G.autoAttack = true
     _G.autoRebirth = true
+    _G.autoBWeapon = true
+    _G.autoBPet = true
 
     --function
     
@@ -27,9 +29,59 @@ if game.PlaceId == 18153052081 then
     function autoBoss1()
         while _G.autoBoss1 == true do
             local args = {
+                [1] = 1
+            }
+            
+            game:GetService("ReplicatedStorage").Events.MapBoss.HurtEnemyEvent:FireServer(unpack(args))
+            local args = {
                 [1] = 2
             }
             
+            game:GetService("ReplicatedStorage").Events.MapBoss.HurtEnemyEvent:FireServer(unpack(args))
+            local args = {
+                [1] = 3
+            }
+            
+            game:GetService("ReplicatedStorage").Events.MapBoss.HurtEnemyEvent:FireServer(unpack(args))
+            local args = {
+                [1] = 4
+            }
+            
+            game:GetService("ReplicatedStorage").Events.MapBoss.HurtEnemyEvent:FireServer(unpack(args))
+            local args = {
+                [1] = 5
+            }
+
+            game:GetService("ReplicatedStorage").Events.MapBoss.HurtEnemyEvent:FireServer(unpack(args))
+            local args = {
+                [1] = 6
+            }
+            
+            game:GetService("ReplicatedStorage").Events.MapBoss.HurtEnemyEvent:FireServer(unpack(args))
+            local args = {
+                [1] = 7
+            }
+            
+            game:GetService("ReplicatedStorage").Events.MapBoss.HurtEnemyEvent:FireServer(unpack(args))
+            local args = {
+                [1] = 8
+            }
+            
+            game:GetService("ReplicatedStorage").Events.MapBoss.HurtEnemyEvent:FireServer(unpack(args))
+            local args = {
+                [1] = 9
+            }
+            
+            game:GetService("ReplicatedStorage").Events.MapBoss.HurtEnemyEvent:FireServer(unpack(args))
+            local args = {
+                [1] = 10
+            }
+
+            game:GetService("ReplicatedStorage").Events.MapBoss.HurtEnemyEvent:FireServer(unpack(args))
+            local args = {
+                [1] = 11
+            }
+
             game:GetService("ReplicatedStorage").Events.MapBoss.HurtEnemyEvent:FireServer(unpack(args))
             wait(0.00000000000000000000000001)
     end 
@@ -53,6 +105,23 @@ if game.PlaceId == 18153052081 then
     end
     end
 
+    function autoBPet()
+        while _G.autoBPet == true do
+            game:GetService("ReplicatedStorage").Events.Pets.EquipBestEvent:FireServer()
+            wait(1)
+        end
+    end
+
+    function autoBWeapon()
+        while _G.autoBWeapon == true do
+            local args = {
+                [1] = 1
+            }
+            
+            game:GetService("ReplicatedStorage").Events.Equip.EquipBestEvent:FireServer(unpack(args))
+            wait(1)
+        end
+    end
     
 
     --tab
@@ -101,6 +170,22 @@ if game.PlaceId == 18153052081 then
         Callback = function(Value)
             _G.autoRebirth = (Value)
             autoRebirth()
+        end    
+    })
+    Ftab:AddToggle({
+        Name = "Auto Best Pet",
+        Default = false,
+        Callback = function(Value)
+            _G.autoBPet = (Value)
+            autoBPet()
+        end    
+    })
+    Ftab:AddToggle({
+        Name = "Auto Best Weapon",
+        Default = false,
+        Callback = function(Value)
+            _G.autoBWeapon = (Value)
+            autoBWeapon()
         end    
     })
 
